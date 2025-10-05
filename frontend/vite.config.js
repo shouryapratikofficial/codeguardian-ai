d59a3e9259e13f081b6edc6bfdc8ba3ade14e1ce
shouryapratikofficial <shouryapratikofficial@gmail.com>
2025-10-05 18:53:07 +0530

    last time

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+ 
 });
